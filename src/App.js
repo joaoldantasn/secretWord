cf9a081b6024c37b9aea6458d85869c7d542c529
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-04-14 13:39:08 -0300

    Alternando estágios do jogo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+//CSS
 import './App.css';
 
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+//data
+import { wordsList } from './data/words';
+
+//components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  //starts the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
